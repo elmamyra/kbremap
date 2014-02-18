6b801b0be7edd5844892c20f3f30304c2a33b7cd
elmamyra <elmamyra@gmail.com>
2014-02-18 23:20:57 +0100

    add separator widget

--- util.py
+++ util.py
@@ -1,5 +1,5 @@
-from PySide.QtGui import QColor
-from PyQt4.QtCore import  QSettings, QSize
+from PySide.QtGui import QColor, QFrame
+from PyQt4.QtCore import  QSettings, QSize, Qt
 import data as d
 import weakref
 
@@ -24,7 +24,28 @@ def saveDialogSize(dialog, key, default=QSize()):
         if dialog:
             QSettings().setValue(key, dialog.size())
     dialog.finished.connect(save)
-    
+
+class Separator(QFrame):
+    def __init__(self, *args, **kwargs):
+        QFrame.__init__(self, *args, **kwargs)
+        self.setLineWidth(1)
+        self.setMidLineWidth(0)
+        self.setOrientation(Qt.Horizontal)
+        
+    def setOrientation(self, orientation):
+        if orientation == Qt.Vertical:
+            self.setFrameShape(QFrame.VLine)
+            self.setFrameShadow(QFrame.Sunken)
+            self.setMinimumSize(2, 0)
+        else:
+            self.setFrameShape(QFrame.HLine)
+            self.setFrameShadow(QFrame.Sunken)
+            self.setMinimumSize(0, 2)
+        self.updateGeometry()
+        
+    def orientation(self):
+        return Qt.Vertical if self.frameStyle() & QFrame.VLine == QFrame.VLine else Qt.Horizontal
+
     
 def modXmask(mod):
     return d.MODIFIER_MASK.get(mod, -1)
