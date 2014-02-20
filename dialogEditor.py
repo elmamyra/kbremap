848dfcec2b94f3f5411f77a8f1cfa8ee96ae8a3a
elmamyra <elmamyra@gmail.com>
2014-02-20 13:09:50 +0100

    move IconButton to widgets.py and other improvements

--- dialogEditor.py
+++ dialogEditor.py
@@ -5,34 +5,18 @@ Dialog to add or edit a key.
 """
 
 from PySide.QtGui import *
-from PySide.QtCore import QSize, Qt, QSettings
-import os
+from PySide.QtCore import Qt
+from pageEditor import *
 import util
 import data as d
 import icons
 
 
-class IconButton(QToolButton):
-    def __init__(self, parent=None):
-        super(IconButton, self).__init__(parent)
-        self.iconPath = ''
-        act = QAction(self,  triggered=self.slotTriggered)
-        self.setDefaultAction(act)
-        
-    def slotTriggered(self):
-        iconPath = QSettings().value('icon-path', os.path.join('/usr', 'share', 'icons'))
-        exts = ['*.'+str(ext) for ext in QImageWriter.supportedImageFormats()]
-        extStr = self.tr("Icon Files") + " ({})".format(' '.join(exts))
-        iconFile = QFileDialog.getOpenFileName(self, self.tr("Icon"), iconPath, extStr)[0]
-        if iconFile:
-            self.defaultAction().setIcon(QIcon(iconFile))
-            self.defaultAction().setData(iconFile)
-            
-    def iconFile(self):
-        return self.defaultAction().data()
+
+
 
 class DialogEditor(QDialog):
-    def __init__(self, parent):
+    def __init__(self, parent, mItem=None):
         super(DialogEditor, self).__init__(parent)
         self.setWindowTitle(self.tr("key editor"))
         layout = QVBoxLayout()
@@ -49,12 +33,12 @@ class DialogEditor(QDialog):
         layoutGroupBox.setFormAlignment(Qt.AlignHCenter|Qt.AlignLeft)
         self.radioIcon = QRadioButton(self.tr("Icon:"), checked=True)
         self.radioText = QRadioButton(self.tr("text:"))
-        self.iconButton = IconButton()
         self.textLineEdit = QLineEdit()
+        self.iconChooser = IconChooser()
         
-        self.iconButton.setIconSize(QSize(32, 32))
         
-        layoutGroupBox.addRow(self.radioIcon, self.iconButton)
+        
+        layoutGroupBox.addRow(self.radioIcon, self.iconChooser)
         layoutGroupBox.addRow(self.radioText, self.textLineEdit)
         
         buttonBox = QDialogButtonBox(QDialogButtonBox.Cancel | QDialogButtonBox.Ok,
@@ -63,6 +47,7 @@ class DialogEditor(QDialog):
         layout.addLayout(top)
         layout.addWidget(self.stackedWid)
         layout.addWidget(displayGroupBox)
+        layout.addStretch(1)
         layout.addWidget(util.Separator())
         layout.addWidget(buttonBox)
         self.setLayout(layout)
@@ -84,8 +69,55 @@ class DialogEditor(QDialog):
             
         self.typeButton.setMenu(menu)
         
-        menu.triggered.connect(self.slotTypeChanged)
+        self.typeToPage = {d.TEXT: PageText(),
+                           d.LAUNCHER: PageLauncher(),
+                           d.SHORTCUT: PageShortcut()
+                        }
         
+        for page in self.typeToPage.values():
+            self.stackedWid.addWidget(page)
+        
+        self.pageEmpty = PageEmpty()
+        self.stackedWid.addWidget(self.pageEmpty)
+        
+        if mItem is not None:
+            self.load(mItem)
+        else:
+            self.currentType = -1
+            self.stackedWid.setCurrentWidget(self.pageEmpty)
+        
+        
+        menu.triggered.connect(self.slotTypeChanged)
+    
+    def load(self, mItem):
+        pass
+    
+    def accept(self):
+        page = self.stackedWid.currentWidget()
+        if self.currentType == -1:
+            QMessageBox.warning(self, self.tr("Warning"), self.tr("You must select a type of key."))
+            return
+        
+        if not page.isValid():
+            QMessageBox.warning(self, self.tr("Warning"), page.errorMessage())
+            return
+        self.displayType = d.GR_ICON if self.radioIcon.isChecked() else d.GR_TEXT
+        self.displayValue = self.iconChooser.iconFile() if self.displayType == d.GR_ICON else self.textLineEdit.text()
+        if not self.displayValue:
+            if self.displayType == d.GR_ICON:
+                mess = self.tr("You must select an icon")
+            else:
+                mess = self.tr("You must enter a text")
+            QMessageBox.warning(self, self.tr("Warning"), mess)
+            return
+        self.data = self.currentType, page.data(), self.displayType, self.displayValue
+        self.done(QDialog.Accepted)
+    
+    def getData(self):
+        return self.data
+    
     def slotTypeChanged(self, act):
+        self.stackedWid.setCurrentWidget(self.typeToPage.get(act.data()) or self.pageEmpty)
         self.typeButton.setText(act.text())
         self.typeButton.setIcon(act.icon())
+        self.currentType = act.data()
