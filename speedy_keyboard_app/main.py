2f25f2328a65a75f4a226c9a06021528c71c730e
elmamyra <elmamyra@gmail.com>
2014-03-02 14:19:21 +0100

    add pause and resume options

--- speedy_keyboard_app/main.py
+++ speedy_keyboard_app/main.py
@@ -6,16 +6,24 @@ parser = argparse.ArgumentParser()
 
 parser.add_argument("-d", "--daemon", action="store_true",
                     help="run the deamon")
+parser.add_argument("-p", "--pause", action="store_true",
+                    help="pause the daemon")
+parser.add_argument("-r", "--resume", action="store_true",
+                    help="pause the daemon")
 parser.add_argument("-q", "--quit", action="store_true",
                     help="close the daemon")
 
 
 args = parser.parse_args()
-if args.quit:
-    daemon.close()
-
-elif args.daemon:
+if args.daemon:
     daemon.start()
+elif args.quit:
+    daemon.close()
+elif args.pause:
+    daemon.pause()
+elif args.resume:
+    daemon.resume()
+
 else:
     mainWindow.start()
 
