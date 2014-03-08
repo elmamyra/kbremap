564be0402d684c6ea461baf25244a8d976a97991
elmamyra <elmamyra@gmail.com>
2014-03-08 18:36:47 +0100

    change options

--- speedy_keyboard_app/main.py
+++ speedy_keyboard_app/main.py
@@ -1,29 +1,26 @@
 import argparse
 import daemon
-import mainWindow
-# print sys.argv
+
+
 parser = argparse.ArgumentParser()
 
-parser.add_argument("-d", "--daemon", action="store_true",
-                    help="run the deamon")
+parser.add_argument("-s", "--start", action="store_true",
+                    help="start the server")
 parser.add_argument("-p", "--pause", action="store_true",
-                    help="pause the daemon")
-parser.add_argument("-r", "--resume", action="store_true",
-                    help="pause the daemon")
+                    help="pause/resume the server")
 parser.add_argument("-q", "--quit", action="store_true",
-                    help="close the daemon")
+                    help="close the server")
 
 
 args = parser.parse_args()
-if args.daemon:
+if args.start:
     daemon.start()
 elif args.quit:
     daemon.close()
 elif args.pause:
     daemon.pause()
-elif args.resume:
-    daemon.resume()
 
 else:
+    import mainWindow
     mainWindow.start()
 
