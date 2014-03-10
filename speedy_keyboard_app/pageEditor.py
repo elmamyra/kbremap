4c0808bae5f034721882a55a587043a19e352e88
elmamyra <elmamyra@gmail.com>
2014-03-10 16:05:35 +0100

    remove remapping page

--- speedy_keyboard_app/pageEditor.py
+++ speedy_keyboard_app/pageEditor.py
@@ -1,12 +1,11 @@
 
-from PySide.QtGui import *
+from PySide.QtGui import *  # @UnusedWildImport
 from PySide.QtCore import Qt
 from widgets import ShortcutWidget
-from speedy_keyboard_app.Xtools import keyGroups
+# from speedy_keyboard_app.Xtools import keyGroups
 import os
-from gtk import gdk
 import mapping
-from Xtools import display
+# from Xtools import display
 
 class PageBase(QWidget):
     def __init__(self, parent=None):
@@ -130,69 +129,9 @@ class PageShortcut(PageBase):
     
     def errorMessage(self):
         return self.tr("You must enter a shortcut.")   
-
         
 
-class PageRemapping(PageBase):
-    def __init__(self, dialog):
-        PageBase.__init__(self)
-        self.dialog = dialog
-        self.menuButton = QPushButton(self.tr("Choose..."))
-        self.layout.addWidget(self.menuButton)
-        self.layout.addStretch(1)
-        self.fillMenu()
-#         self.char = ''
-#         self.keyname = ''
-        self.keysym = -1
-        
-    def fillMenu(self):
-        menu = QMenu(self.menuButton)
-        for group, data in keyGroups.keyGroups:
-            m = menu.addMenu(group)
-            for keyname, keysym, char_ in data:
-                char, name = self.keysym2charAndName(keysym)
-                a = m.addAction(u"{}  {}".format(char, name))
-                a.setData(keysym)
-                
-        self.menuButton.setMenu(menu)
-        menu.triggered.connect(self.slotMenu)
-    
-    def setMenuTitle(self, char, keyname):
-        self.menuButton.setText(u"{}  {}".format(char, keyname))
-    
-    def slotMenu(self, act):
-        self.menuButton.setText(act.text())
-        self.keysym = act.data()
-#         self.refreshMenuText()
-        char = display.keysym2char(self.keysym)
-        if char:
-            self.dialog.textEvent.emit(char)
-    
-#     def keysym2charName(self, keysym):
-#         name = gdk.keyval_name(keysym) or ""  # @UndefinedVariable
-#         char = gdk.keyval_to_unicode(keysym)  # @UndefinedVariable
-#         char = unichr(char) if char else ""
-#         return char, name
 
-    def keysym2charAndName(self, keysym):
-        return display.keysym2char(keysym), display.keysym2name(keysym)
-
-    def setData(self, data):
-        char, name = self.keysym2charAndName(data)
-        self.menuButton.setText(u"{}  {}".format(char, name))
-#         self.keyname, self.char = data
-#         self.refreshMenuText()
-        
-    def data(self):
-        return self.keysym
-    
-    def isValid(self):
-        return self.keysym != -1
-    
-    def errorMessage(self):
-        return self.tr("You must choose a keysym.")
-        
-        
 class PageLoad(PageBase):
     def __init__(self, dialog):
         PageBase.__init__(self)
@@ -209,44 +148,7 @@ class PageLoad(PageBase):
         index = self.combo.currentIndex()
         return self.combo.itemData(index)
             
-        
-        
-# class PageServer(PageBase):
-#     def __init__(self, dialog):
-#         PageBase.__init__(self)
-#         self.dialog = dialog
-#         self.combo = QComboBox()
-#         self.layout.addWidget(self.combo)
-#         self.li = (('media-playback-start', self.tr("Resume"), 'resume'),
-#               ('media-playback-pause', self.tr("Pause"), 'pause'),
-#               ('media-playback-stop', self.tr("Stop"), 'quit'),
-#               ('sync', self.tr("synchronize"), 'update')
-#               )
-#               
-#         for icon, name, data in self.li:
-#             self.combo.addItem(icons.get(icon), name, data)
-#         
-#         self.action = None
-#         self.combo.currentIndexChanged.connect(self.slotCombo)
-#     
-#     def start(self):
-#         index = self.combo.currentIndex()
-#         self.slotCombo(index)
-#         
-#         
-#     def setData(self, data):
-#         index = self.combo.findData(data)
-#         self.combo.setCurrentIndex(index)
-#         
-#     def data(self):
-#         index = self.combo.currentIndex()
-#         return self.combo.itemData(index)
-#     
-#     def slotCombo(self, index):
-#         iconName = self.li[index][0]
-#         self.dialog.iconEvent.emit(iconName)
-#         
-            
+
         
     
     
