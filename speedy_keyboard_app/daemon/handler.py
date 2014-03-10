91acf2bd452e35fc236d88a7cb4b7b9d95e62b64
elmamyra <elmamyra@gmail.com>
2014-03-10 16:01:01 +0100

    change remapping system

--- speedy_keyboard_app/daemon/handler.py
+++ speedy_keyboard_app/daemon/handler.py
@@ -6,6 +6,7 @@ from subprocess import Popen, PIPE
 from .. import data as d
 from .. import logger
 import socket, sys
+
 # from speedy_keyboard_app import mainWindow
 PORT = 22347
 
@@ -22,13 +23,16 @@ class Handler(object):
         self.ungrabKeys()
         self.mapping = mapping.Mapping()
         self.mapping.loadCurrent()
-        self.log.handler_('load mapping (%s)', self.mapping.title() or 'none')
+        self.log.handler_('load mapping (%s)', self.mapping.title or 'none')
+        self.restoreMap()
         self.grabKeys()
+        self.remapKeys()
     
     def start(self):
         self.mapping.loadCurrent()
-        self.log.handler_('start (%s)', self.mapping.title() or 'none')
+        self.log.handler_('start (%s)', self.mapping.title or 'none')
         self.grabKeys()
+        self.remapKeys()
         th = threading.Thread(target=self.eventThread)
         th.start()
     
@@ -42,64 +46,50 @@ class Handler(object):
         except socket.error, msg_:
             self.log.critical('handle connection failed: %s', msg_)
             
-    def grabKeys(self, exceptType=-1):
+    def grabKeys(self, exceptType=()):
         self.log.handler_('grab keys')
-#         remapKeyList = []
-        for item in self.mapping:
-#             if item.type == d.REMAPPING:
-#                 remapKeyList.append((item.keycode, item.modifiers, item.data))
-#             self.display.remapKey(item.keycode, item.modifiers, item.data[0])
-#             else:
-            self.display.grabKey(item.keycode, item.modifiers)
+        for item in self.mapping.shortcut:
+            if item.type not in exceptType:
+                self.display.grabKey(item.keycode, item.modifiers)
                 
-#         self.display.remapKeys(remapKeyList)
-            
-    def ungrabKeys(self, exceptType=-1):
+    def ungrabKeys(self, exceptType=()):
         self.log.handler_('ungrab keys')
-        for item in self.mapping:
-            self.display.ungrabKey(item.keycode, item.modifiers)
+        for item in self.mapping.shortcut:
+            if item.type not in exceptType:
+                self.display.ungrabKey(item.keycode, item.modifiers)
             
-#         self.display.resetMapping()
-        
+    def remapKeys(self):
+        for item in self.mapping.remap:
+            self.display.remapKey(item.keycode, item.keysyms)
+            
+    def restoreMap(self):
+        self.display.resetMapping()
     
-
-#     def grabKey(self, item):
-#         if item.type == d.REMAPPING:
-#                 self.display.remapKey(item.keycode, item.modifiers, item.data[0])
-#         else:
-#             self.display.grabKey(item.keycode, item.modifiers)
-#         
-#     def ungrabKey(self, item):
-#         self.display.ungrabKey(item.keycode, item.modifiers)
-#             
-#         
-    
-    
+            
     def stop(self):
         self.running = False
         self.display.resetMapping()
         
     def pause(self):
         self.ungrabKeys(exceptType=(d.PAUSE,))
+        self.restoreMap()
     
     def resume(self):
-        self.grabKey((d.PAUSE,))
-#         for item in self.mapping:
-#             if item.type != d.PAUSE:
-#                 self.grabKey(item)
+        self.grabKeys((d.PAUSE,))
+        self.remapKeys()
     
     def eventThread(self):
         while self.running:
             event = self.display.nextKeyEvent()
             if event:
                 modMask = self.display.removeNumLockMask(event.keycode, event.modifiers)
-                item = self.mapping.getItem(event.keycode, modMask)
+                item = self.mapping.shortcut[event.keycode, modMask]
                 if item:
                     self.log.handler_('item send: %s', item.type)
                     if item.type == d.TEXT:
                         self.display.sendText(item.data)
                     elif item.type == d.SHORTCUT:
-                        self.display.sendKeycode(item.data[0], item.data[1])
+                        self.display.sendEntry(item.data[0], item.data[1])
                     elif item.type == d.COMMAND:
                         Popen(item.data.split(), close_fds=True)
                     elif item.type == d.REMAPPING:
