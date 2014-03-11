413e2c2702eaca93fedada74a4ca73780010f010
elmamyra <elmamyra@gmail.com>
2014-03-11 13:08:27 +0100

    add debug option

--- speedy_keyboard_app/main.py
+++ speedy_keyboard_app/main.py
@@ -10,15 +10,19 @@ parser.add_argument("-p", "--pause", action="store_true",
                     help="pause/resume the server")
 parser.add_argument("-q", "--quit", action="store_true",
                     help="close the server")
+parser.add_argument("-d", "--debug", action="store_true", 
+                    help="show log in console")
 
 
 args = parser.parse_args()
+
 if args.start:
-    daemon.start()
+    daemon.start(args.debug)
 elif args.quit:
     daemon.close()
 elif args.pause:
     daemon.pause()
+
 
 else:
     import mainWindow
