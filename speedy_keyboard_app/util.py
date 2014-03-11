c470630ed9b942a2bd9a24e1ab615d196e290106
elmamyra <elmamyra@gmail.com>
2014-03-11 21:28:47 +0100

    move Separator from util to widgets

--- speedy_keyboard_app/util.py
+++ speedy_keyboard_app/util.py
@@ -27,35 +27,11 @@ def saveDialogSize(dialog, key, default=QSize()):
             QSettings().setValue(key, dialog.size())
     dialog.finished.connect(save)
 
-class Separator(QFrame):
-    def __init__(self, *args, **kwargs):
-        QFrame.__init__(self, *args, **kwargs)
-        self.setLineWidth(1)
-        self.setMidLineWidth(0)
-        self.setOrientation(Qt.Horizontal)
-        
-    def setOrientation(self, orientation):
-        if orientation == Qt.Vertical:
-            self.setFrameShape(QFrame.VLine)
-            self.setFrameShadow(QFrame.Sunken)
-            self.setMinimumSize(2, 0)
-        else:
-            self.setFrameShape(QFrame.HLine)
-            self.setFrameShadow(QFrame.Sunken)
-            self.setMinimumSize(0, 2)
-        self.updateGeometry()
-        
-    def orientation(self):
-        return Qt.Vertical if self.frameStyle() & QFrame.VLine == QFrame.VLine else Qt.Horizontal
-
-
 def str2bool(v):
     return v.lower() in ('true', '1')
  
 def keyboardColors(name):
     return d.KEYBOARD_COLOR.get(name, QColor())
-
-
 
 def configPath(fileName='mappings.xml'):
         configDir = os.environ.get('XDG_CONFIG_HOME') or os.path.expanduser("~/.config")
