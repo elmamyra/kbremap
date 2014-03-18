b5cf51260a70f87cb6c814fc1d4c4b2bcd645fc0
elmamyra <elmamyra@gmail.com>
2014-03-18 13:14:22 +0100

    add copying

--- kbremap_app/keyTools/__init__.py
+++ kbremap_app/keyTools/__init__.py
@@ -1,21 +1,19 @@
-# This file is part of the speedy-entry project.
+# This file is part of the kbremap project.
+# Copyright (C) 2014 Nicolas Malarmey
 #
-# Copyright (c) 2014 - 2014 by Nicolas Malarmey
-#
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation; either version 2
-# of the License, or (at your option) any later version.
-#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-#
+# 
 # You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
-# See http://www.gnu.org/licenses/ for more information.
+# along with this program.  If not, see http://www.gnu.org/licenses.
+# contact: elmamyra@gmail.com
 
 # -*- coding: utf-8 -*-
 from Xlib.display import Display
